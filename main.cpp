5d1302381d3cb0d273c5a1c165206d093ced033c
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-03 17:36:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 int main() {
+   
+   int lista[10];
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+    int a=0;
+    cin >> a;
+
+    // Inserire soluzione
+    int b=0, c=0;
+    for(int i=0;i<10;i++){
+        if(a<lista[i]){
+            b=lista[i];
+            lista[i]=a;
+            a=b;
+
+        }else if(lista[i]>=a){
+
+        }
+    }
+
+
+    for(int i=0;i<10;i++){
+        cout << lista[i] << " ";
+    }
+   
    return 0;
 }
